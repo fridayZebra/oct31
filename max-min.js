b215d4f4a9826af0a49731fe4a4e9c1c56ea7780
Ryan Sabo <sabora920@gmail.com>
2017-10-31 13:50:35 -0400

    added the last problems

--- max-min.js
+++ max-min.js
@@ -1,38 +1,63 @@
 'use strict';
 const numbers = [1,2,3,4,5,566,7,7,3,2];
 
-function max(numbers) {
-  let maxNum = 0; //numbers[0];
-  let i = 0;
+// function max(numbers) {
+//   let maxNum = 0; //numbers[0];
+//   let i = 0;
 
-  while (i < numbers.length) {
-    if (numbers[i] > maxNum) {
-      maxNum = numbers[i];
-    }  i++;    
-  } return maxNum;
+//   while (i < numbers.length) {
+//     if (numbers[i] > maxNum) {
+//       maxNum = numbers[i];
+//     }  i++;    
+//   } return maxNum;
 
-} console.log(max(numbers));
-
-
-function min(numbers) {
-  let minNum = numbers[0];
-  let i = 0;
-
-  while (i < numbers.length) {
-    if (numbers[i] < minNum) {
-      minNum = numbers[i];
-    }  i++;    
-  } return minNum;
-
-} console.log(min(numbers));
+// } console.log(max(numbers));
 
 
-// numbers=[1,2,3,4,5,6]
+// function min(numbers) {
+//   let minNum = numbers[0];
+//   let i = 0;
 
-// function findMax(numbers){
-//    maxNum = 0; 
-//      i=0 
-//    while(i<numbers.length) {
-//  
-//  if(numbers[i]>maxNum){ maxNum = numbers[i]; } i++; 
-// } 
+//   while (i < numbers.length) {
+//     if (numbers[i] < minNum) {
+//       minNum = numbers[i];
+//     }  i++;    
+//   } return minNum;
+
+// } console.log(min(numbers));
+
+
+function average(numbers){
+  let sum = 0;
+  let i=0;
+  while(i<numbers.length){
+    sum += numbers[i];
+    i++;
+  }
+  let result = sum / numbers.length;
+  return result;
+}
+
+console.log(average(numbers));
+
+const fizzBuzzNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+function fizzBuzz(fizzBuzzNumbers){
+
+  for(let i=0; i<fizzBuzzNumbers.length; i++){
+
+    switch(true){
+      case (fizzBuzzNumbers[i]%15===0):
+        fizzBuzzNumbers[i] = 'fizzbuzz';
+        break;
+      case (fizzBuzzNumbers[i]%5===0):
+        fizzBuzzNumbers[i] = 'buzz';
+        break;
+      case (fizzBuzzNumbers[i]%3===0):
+        fizzBuzzNumbers[i] = 'fizz';
+        break;
+    }
+  }
+  return fizzBuzzNumbers;
+}
+console.log(fizzBuzz(fizzBuzzNumbers));
