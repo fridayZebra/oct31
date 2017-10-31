73370ddf98748a86106470fd331a00e04939dbb5
Firoz Kamdar <firozkamdar@gmail.com>
2017-10-31 11:25:07 -0700

    completed through first higher order function drill

--- max-min.js
+++ max-min.js
@@ -1,63 +1,80 @@
-'use strict';
-const numbers = [1,2,3,4,5,566,7,7,3,2];
+// 'use strict';
+// // const numbers = [1,2,3,4,5,566,7,7,3,2];
+// // // Min-Max -----------------
+// // // function max(numbers) {
+// // //   let maxNum = 0; //numbers[0];
+// // //   let i = 0;
 
-// function max(numbers) {
-//   let maxNum = 0; //numbers[0];
-//   let i = 0;
+// // //   while (i < numbers.length) {
+// // //     if (numbers[i] > maxNum) {
+// // //       maxNum = numbers[i];
+// // //     }  i++;    
+// // //   } return maxNum;
 
-//   while (i < numbers.length) {
-//     if (numbers[i] > maxNum) {
-//       maxNum = numbers[i];
-//     }  i++;    
-//   } return maxNum;
+// // // } console.log(max(numbers));
 
-// } console.log(max(numbers));
+// // // function min(numbers) {
+// // //   let minNum = numbers[0];
+// // //   let i = 0;
 
+// // //   while (i < numbers.length) {
+// // //     if (numbers[i] < minNum) {
+// // //       minNum = numbers[i];
+// // //     }  i++;    
+// // //   } return minNum;
 
-// function min(numbers) {
-//   let minNum = numbers[0];
-//   let i = 0;
+// // // } console.log(min(numbers));
 
-//   while (i < numbers.length) {
-//     if (numbers[i] < minNum) {
-//       minNum = numbers[i];
-//     }  i++;    
-//   } return minNum;
+// // // Average -----------------
+// // function average(numbers){
+// //   let sum = 0;
+// //   let i=0;
+// //   while(i<numbers.length){
+// //     sum += numbers[i];
+// //     i++;
+// //   }
+// //   let result = sum / numbers.length;
+// //   return result;
+// // }
 
-// } console.log(min(numbers));
+// // console.log(average(numbers));
 
+// // // FizzBuzz -----------------
+// // const fizzBuzzNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-function average(numbers){
-  let sum = 0;
-  let i=0;
-  while(i<numbers.length){
-    sum += numbers[i];
-    i++;
-  }
-  let result = sum / numbers.length;
-  return result;
-}
+// // function fizzBuzz(fizzBuzzNumbers){
 
-console.log(average(numbers));
+// // //   for(let i=0; i<fizzBuzzNumbers.length; i++){
 
-const fizzBuzzNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// // //     switch(true){
+// // //       case (fizzBuzzNumbers[i]%15===0):
+// // //         fizzBuzzNumbers[i] = 'fizzbuzz';
+// // //         break;
+// // //       case (fizzBuzzNumbers[i]%5===0):
+// // //         fizzBuzzNumbers[i] = 'buzz';
+// // //         break;
+// // //       case (fizzBuzzNumbers[i]%3===0):
+// // //         fizzBuzzNumbers[i] = 'fizz';
+// // //         break;
+// // //     }
+// // //   }
+// // //   return fizzBuzzNumbers;
+// // // }
+// // // console.log(fizzBuzz(fizzBuzzNumbers));
 
-function fizzBuzz(fizzBuzzNumbers){
+// // ----------Higher Order Function Drills -----------------
+// //Functions as arguments (1)
+// function repeat(fn, n) {
+//   for (let i = 0; i < n; i++) {
+//     fn();
+//   }
+// }
+// function hello() {
+//   console.log(`Hello world.`);
+// }
 
-  for(let i=0; i<fizzBuzzNumbers.length; i++){
+// function goodBye() {
+//   console.log(`Goodbye world.`);
+// }
 
-    switch(true){
-      case (fizzBuzzNumbers[i]%15===0):
-        fizzBuzzNumbers[i] = 'fizzbuzz';
-        break;
-      case (fizzBuzzNumbers[i]%5===0):
-        fizzBuzzNumbers[i] = 'buzz';
-        break;
-      case (fizzBuzzNumbers[i]%3===0):
-        fizzBuzzNumbers[i] = 'fizz';
-        break;
-    }
-  }
-  return fizzBuzzNumbers;
-}
-console.log(fizzBuzz(fizzBuzzNumbers));
+// console.log(repeat(goodBye,5));
